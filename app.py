045fcc1c99c16fc014b4b498b3629944669bda7a
arek <alek123252@gmail.com>
2024-11-15 17:28:06 +0100

    Get txt file
    wrote a function that takes a text file from the given url and saves it
    to arytkul.txt

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from gpt_client import GPTClient
+from get_text_file import get_text_file
+import sys
 
-if __name__ == "__main__":
+sys.stdout.reconfigure(encoding='utf-8') # to prevent UnicodeEncodeError
+
+def main():
     gpt_client = GPTClient()
     message = "Hello, GPT!"
     response = gpt_client.gpt_response(message)
     if response:
         print(response)
+    text_file = get_text_file()
+    print(text_file)
+
+if __name__ == "__main__":
+    main()
---
+++ get_text_file.py
@@ -0,0 +1,17 @@
+import requests
+
+URL = "https://cdn.oxido.pl/hr/Zadanie%20dla%20JJunior%20AI%20Developera%20-%20tresc%20artykulu.txt"
+SAVE_PATH="artykul.txt"
+
+def get_text_file():
+    try:
+        response = requests.get(URL)
+        if response.status_code == 200:
+            with open(SAVE_PATH, 'w', encoding='utf-8') as file:
+                file.write(response.text)
+            print(f"File saved to {SAVE_PATH}")
+            return response.text
+        else:
+            print(f"Download error: {response.status_code}")
+    except Exception as e:
+        print(f"Error: {e}")
